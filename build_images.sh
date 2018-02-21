fff6cc1b50655192373c8f8a76d1bfb3aef74be0
Jonas Neubert <jn@jonasneubert.com>
2018-02-20 22:15:33 -0800

    add images for Python 3.4.7 and 3.5.4

--- build_images.sh
+++ build_images.sh
@@ -5,6 +5,16 @@ docker build -f python3.6.4-mono5.2.0.224-pythonnet2.4.0.dev0.Dockerfile -t jone
 docker build -f python3.6.4-mono4.8.0.524-pythonnet2.4.0.dev0.Dockerfile -t jonemo/pythonnet:python3.6.4-mono4.8.0.524-pythonnet2.4.0.dev0 .
 docker build -f python3.6.4-mono4.8.0.524-pythonnet2.3.0.Dockerfile -t jonemo/pythonnet:python3.6.4-mono4.8.0.524-pythonnet2.3.0 .
 
+docker build -f python3.5.4-mono4.8.0.524-pythonnet2.3.0.Dockerfile -t jonemo/pythonnet:python3.5.4-mono4.8.0.524-pythonnet2.3.0 .
+docker build -f python3.5.4-mono4.8.0.524-pythonnet2.4.0.dev0.Dockerfile -t jonemo/pythonnet:python3.5.4-mono4.8.0.524-pythonnet2.4.0.dev0 .
+docker build -f python3.5.4-mono5.2.0.224-pythonnet2.4.0.dev0.Dockerfile -t jonemo/pythonnet:python3.5.4-mono5.2.0.224-pythonnet2.4.0.dev0 .
+docker build -f python3.5.4-mono5.4.1.6-pythonnet2.4.0.dev0.Dockerfile -t jonemo/pythonnet:python3.5.4-mono5.4.1.6-pythonnet2.4.0.dev0 .
+
+docker build -f python3.4.7-mono4.8.0.524-pythonnet2.3.0.Dockerfile -t jonemo/pythonnet:python3.4.7-mono4.8.0.524-pythonnet2.3.0 .
+docker build -f python3.4.7-mono4.8.0.524-pythonnet2.4.0.dev0.Dockerfile -t jonemo/pythonnet:python3.4.7-mono4.8.0.524-pythonnet2.4.0.dev0 .
+docker build -f python3.4.7-mono5.2.0.224-pythonnet2.4.0.dev0.Dockerfile -t jonemo/pythonnet:python3.4.7-mono5.2.0.224-pythonnet2.4.0.dev0 .
+docker build -f python3.4.7-mono5.4.1.6-pythonnet2.4.0.dev0.Dockerfile -t jonemo/pythonnet:python3.4.7-mono5.4.1.6-pythonnet2.4.0.dev0 .
+
 docker build -f python2.7.14-mono4.8.0.524-pythonnet2.3.0.Dockerfile -t jonemo/pythonnet:python2.7.14-mono4.8.0.524-pythonnet2.3.0 .
 docker build -f python2.7.14-mono4.8.0.524-pythonnet2.4.0.dev0.Dockerfile -t jonemo/pythonnet:python2.7.14-mono4.8.0.524-pythonnet2.4.0.dev0 .
 docker build -f python2.7.14-mono5.2.0.224-pythonnet2.4.0.dev0.Dockerfile -t jonemo/pythonnet:python2.7.14-mono5.2.0.224-pythonnet2.4.0.dev0 .
